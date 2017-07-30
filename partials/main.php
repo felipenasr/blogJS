ac1861ad05e7e3f47c958918e422a4fcf58a96ad
felipenasr <felipenriccitrab@gmail.com>
2017-07-30 00:21:25 -0300

    funcao que dispara verificacao, autenticacao e gravacao de dados ja esta pronta. desenvolvendo funcao de validacao de todos os dados

--- partials/main.php
+++ partials/main.php
@@ -2,35 +2,51 @@
 	<form class="col-s12" action="#">
 		<div class="row">
 			<div class="input-field col s12">
-				<input id="titulo" type="text">
-				<label for="titule">Título</label>
+				<input id="titulo" type="text" data-length="70">
+				<label for="titule">título</label>
 			</div>
 			<div class="input-field col s12">
-				<input id="sub-titulo" type="text">
-				<label for="sub-titulo">Subtitulo</label>
-			</div>
-
-			<div class="input-field col s12">
-				<textarea id="text-input" class="materialize-textarea"></textarea>
-				<label for="text-input">Texto</label>
+				<input id="sub-titulo" type="text" data-length="150">
+				<label for="sub-titulo">subtitulo</label>
 			</div>
 
 			<div class="input-field col s12">
-				<input id="name" type="text">
-				<label for="name">Nome</label>
+				<textarea id="text-input" class="materialize-textarea" data-length="2500"></textarea>
+				<label for="text-input">texto</label>
 			</div>
 
 			<div class="input-field col s12">
-				<input id="email" type="text">
-				<label for="email">E-mail</label>
+				<input id="imagem" type="text" maxlength=1060 >
+				<label for="imagem">coloque o link da sua imagem</label>
 			</div>
 
+			<div class="input-field col s6">
+				<input id="name" maxlength=45 type="text">
+				<label for="name" >nome</label>
+			</div>
+
+			<div class="input-field col s6">
+				<input id="email" maxlength=60 type="email">
+				<label for="email" >e-mail</label>
+			</div>
+
+			<div class="input-field col s6">
+				<input id="twitter" maxlength=200 type="text">
+				<label for="twitter">rede social ou contato</label>
+			</div>
+			<div class="input-field col s3">
+				<input id="cidade" maxlength=30 type="text">
+				<label for="cidade" >cidade</label>
+			</div>
+			<div class="input-field col s3">
+				<input id="idade" maxlength=2 type="text">
+				<label for="idade" >idade</label>
+			</div>
 			<div class="input-field col s12">
-				<input id="twitter" type="text">
-				<label for="twitter">Twitter</label>
+				<button class="btn waves-effect waves-light btn-large right" type="submit" name="action" id="submit">Submit
+	    			<i class="material-icons right">send</i>
+	  			</button>
 			</div>
-
-
 		</div>
 	</form>
 </main>
--- src/textcad.js
+++ src/textcad.js
@@ -1,3 +1,43 @@
+// JS do cadastro do texto.
+//
+// passo 1: validar campos;
+// passo 2: gravar dados;
+// passo 3 redirecionar
+
 (function () {
-	document.querySelector('#teste');
+
+	// montando a UI
+	var ui = {
+		fields: document.querySelectorAll('input'),
+		txt: document.querySelector('textarea'),
+		button: document.querySelector('#submit')
+	};
+	var validaCampos = function (e){
+		e.preventDefault();
+		var errors = 0;
+		if (ui.txt.value == ""){
+			$(ui.txt).addClass('error');
+			errors++;
+		}else{
+			$(ui.txt).removeClass('error');
+		}
+		ui.fields.forEach(function(field, key){
+			if (field.value.trim() == "") {
+				$(field).addClass('error');
+				errors++;
+			}else{
+				$(field).removeClass('error');
+			}
+		});
+		if (errors == 0 ){
+      		document.querySelector('.error').focus();
+		}
+	};
+
+
+
+	// funcao gatilho do cadastro
+	var cadastro = function(){
+		ui.button.addEventListener("click", validaCampos);
+	}();
 })();
